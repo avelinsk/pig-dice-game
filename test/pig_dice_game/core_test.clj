7b8933853b9194285ff1109d680e6b49ea9801b4
Angelina Velinska <an.velinska@gmail.com>
2021-01-30 13:49:17 +0100

    Pig Dice Game full on

---
+++ test/pig_dice_game/core_test.clj
@@ -0,0 +1,7 @@
+(ns pig-dice-game.core-test
+  (:require [clojure.test :refer :all]
+            [pig-dice-game.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
